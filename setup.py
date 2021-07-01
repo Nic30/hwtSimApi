915eb9bf16b724fc5375a2ac06da1d8feae104e1
Nic30 <nic30@seznam.cz>
2021-07-01 22:45:43 +0200

    v1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='hwtSimApi',
-    version='1.2',
+    version='1.3',
     description='RTL simulator API',
     long_description=long_description,
     long_description_content_type="text/markdown",
