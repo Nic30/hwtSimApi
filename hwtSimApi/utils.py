4dcc0afa27a0e2a0c8b8220cc17ad080d6b6dfaf
Nic30 <nic30@seznam.cz>
2021-12-26 18:48:34 +0100

    new util: period_to_freq

--- hwtSimApi/utils.py
+++ hwtSimApi/utils.py
@@ -1,5 +1,11 @@
+from typing import Union
+
 from hwtSimApi.constants import Time
 
 
-def freq_to_period(f):
-    return (Time.s / f)
+def freq_to_period(f: Union[float, int]):
+    return Time.s / f
+
+
+def period_to_freq(t: Union[float, int]):
+    return Time.s / float(t)
